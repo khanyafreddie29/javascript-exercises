0add8f18acc9cc5f6c6964c3923391601dd12ffc
Khanya Freddie <khanyafreddie2903@gmail.com>
2025-06-02 14:15:18 +0200

    Update exercise-4.js

---
+++ exercise-4.js
@@ -0,0 +1,10 @@
+let score = 75;
+let passingGrade = 60;
+
+if (score == 100){
+    console.log('perfect 100');  
+} else if (score >= passingGrade) {
+    console.log('Passed');   
+} else {
+    console.log('Failed');  
+}
