dc95f5f00b95a60188d1dcf876592480269c9b7d
khanyafreddie29 <khanyafreddie2903@gmail.com>
2025-06-02 15:37:47 +0200

    javascript-6

---
+++ functions.js
@@ -0,0 +1,21 @@
+function greeting(name){
+    console.log(`hello ${name}`);
+    
+}
+greeting('Zaak')
+
+function add(name, surname){
+    console.log(`hello ${name} ${surname}`);
+}
+add('Mary','Joe');
+
+function Add(name,surname,age){
+    console.log(`${name} ${surname} is ${age} years old`)
+}
+Add('Jamie','Barker', 22)
+
+function add(seventy,eighty){
+    return 200 + seventy + eighty;
+}
+add(70,80);
+console.log(add(70,80));
